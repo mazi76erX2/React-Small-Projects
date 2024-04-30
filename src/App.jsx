d034ad6019e60796edabb8bbe9382c96727211aa
Xolani <mazi76erx@gmail.com>
2024-05-01 01:16:45 +0200

    Set up project files and Project 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import EsignatureApp from "./projects/01-Esignatures/EsignatureApp";
+
+function App() {
+  return (
+    <>
+      <EsignatureApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/projects/01-Esignatures/EsignatureApp.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Title from "../components/Title";
+
+export default function EsignatureApp() {
+  const [name, setName] = useState("Your Signature");
+  const [date, setDate] = useState("");
+
+  const handleNameChange = (e) => {
+    // console.log(e.target.value);
+    setName(e.target.value);
+  };
+
+  const inputStyle = {
+    border: "none",
+    borderBottom: "2px dotted",
+    outline: "none",
+    padding: ".35rem 0",
+  };
+
+  document.body.style.background = "#eee";
+
+  return (
+    <div className="container text-center">
+      <Title classes={"title text-center"} text={"Name"} />
+      <Title classes={"main-title text-center mb-4"} text={"Date"} />
+      <p>
+        Quis duis aliqua culpa do ullamco dolor exercitation in laboris elit
+        labore enim duis anim. Laborum voluptate dolore sint consequat. Deserunt
+        et consectetur reprehenderit amet excepteur sit culpa laboris. Elit
+        incididunt culpa id elit enim. Sunt non enim Lorem amet dolor cillum
+        dolor ea minim aliquip magna. Mollit cillum enim excepteur veniam magna
+        est excepteur deserunt eiusmod fugiat ut veniam laborum laboris. Laborum
+        ipsum anim incididunt esse Lorem cillum do esse anim laborum consequat
+        do aute dolor.
+      </p>
+      <footer
+        className="d-flex"
+        style={{
+          justifyContent: "space-around",
+          position: "relative",
+          top: "40vh",
+        }}
+      >
+        <input type="date" value={date} style={inputStyle}></input>
+        <input
+          type="text"
+          value={name}
+          style={inputStyle}
+          onChange={handleNameChange}
+        ></input>
+      </footer>
+    </div>
+  );
+}
