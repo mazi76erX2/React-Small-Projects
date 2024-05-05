da8759568eca7804619e065e854f78971548377f
Xolani Mazibuko <mazi76erx@gmail.com>
2024-05-05 14:40:54 +0200

    Added LikeMy Photo Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import EsignatureApp from "./projects/01-Esignatures/EsignatureApp";
 import RandomizeColors from "./projects/02-RandomizeColors/RandomizeColors";
+import LikeMyPhoto from "./projects/03-Like-My-Photo/LikeMyPhoto";
 
 function App() {
   return (
     <div className="App">
       {/* <EsignatureApp /> */}
-      <RandomizeColors />
+      {/* <RandomizeColors />
+       */}
+      <LikeMyPhoto />
     </div>
   );
 }
--- src/projects/03-Like-My-Photo/LikeMyPhoto.jsx
+++ src/projects/03-Like-My-Photo/LikeMyPhoto.jsx
@@ -1,16 +1,47 @@
 import React, { useState } from "react";
+
+import { GiGluttonousSmile, GiBugleCall } from "react-icons/gi";
+import { AiOutlineHeart, AiTwotoneHeart } from "react-icons/ai";
+
 import Title from "../components/Title";
 import MinatoImg from "./img/minato-fights-the-tailed-beasts.avif";
 
 export default function LikeMyPhoto() {
-  const [like, setLike] = useState(0);
+  const [like, setLike] = useState(false);
   const [count, setCount] = useState(0);
+
+  const toggleLike = () =>
+    !like
+      ? setLike(true) & setCount(count + 1)
+      : setLike(false) & setCount(count - 1);
 
   return (
     <div className="container text-center">
-      <Title text={"Like My Photo"} classes={"title mb-4"} />
+      <Title text={"Like My Photo"} />
       <Title classes={"subtitle"} text={`Likes: ${count}`} />
-      <div className="card card-dark"></div>
+      <div
+        className="card card-dark m-auto shadow-sm"
+        style={{ width: 300, cursor: "pointer" }}
+      >
+        <div className="card-header fs-xl">
+          <GiGluttonousSmile className="mr-2" />
+          <small>Minato Fights the Tailed Beasts</small>
+        </div>
+        <img
+          src={MinatoImg}
+          alt="Minato Fights the Tailed Beasts"
+          style={{ height: "fit-content" }}
+          onDoubleClick={toggleLike}
+        />
+        <div className="card-footer">
+          <GiBugleCall />
+          {like ? (
+            <AiTwotoneHeart className="text-danger" />
+          ) : (
+            <AiOutlineHeart className="fs-lg" />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
